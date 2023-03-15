43d7193f718615b7edc8fcb796f7ce262ecd4efe
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-03-15 16:08:32 -0300

    refactor: replace standard "for" loop by "for of"

--- src/utils/audioMetadata.js
+++ src/utils/audioMetadata.js
@@ -1,23 +1,23 @@
 const jsmediatags = window.jsmediatags;
 
-const getAudioMetadata = (file) => (
-  new Promise((resolve, reject) => (
-    new jsmediatags.read(file, {
-      onSuccess: function({ tags }) {
-        resolve(tags);
-      },
-      onError: function() {
-        reject(new Error('unable to get audio metadata'));
-      }
-    })
-  ))
-);
+const getAudioMetadata = (file) =>
+  new Promise(
+    (resolve, reject) =>
+      new jsmediatags.read(file, {
+        onSuccess: function ({ tags }) {
+          resolve(tags);
+        },
+        onError: function () {
+          reject(new Error('unable to get audio metadata'));
+        },
+      })
+  );
 
 const handlePicture = (picture) => {
   const { data, format } = picture;
-  let base64String = "";
-  for (let i = 0; i < data.length; i++) {
-    base64String += String.fromCharCode(data[i]);
+  let base64String = '';
+  for (const charCode of data) {
+    base64String += String.fromCharCode(charCode);
   }
   return `data:${format};base64,${window.btoa(base64String)}`;
 };
@@ -27,6 +27,6 @@ const getAudioFileInfos = async (file) => {
   const cover = handlePicture(picture);
   const url = URL.createObjectURL(file);
   return { title, artist, cover, name: file.name, url };
-}
+};
 
 export default getAudioFileInfos;
